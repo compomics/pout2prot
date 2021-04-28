5fa0242956e9bee2de1fe6a1a2435c991aab5fcb
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2021-04-28 08:43:24 +0200

    Started implementation of OutputWriter

--- OutputWriter.py
+++ OutputWriter.py
@@ -1,3 +1,7 @@
 # Input grouping and all other dicts
 # Should produce one Prophane file for groups and one Prophane file for subgroups
 
+def write_to_file(groups, psm_exp, pep_psm, pep_prot, prot_pept, file):
+    with open(file, "w") as f:
+        f.write("sample category\tsample name\tprotein accessions\tspectrum count")
+
