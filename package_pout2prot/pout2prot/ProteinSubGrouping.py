f625c4671b8f856d633c938911fbb3585b8e8908
Kay Schallert <kay.schallert@ovgu.de>
2021-10-26 16:35:14 +0200

    Fixed messed up merge

--- package_pout2prot/pout2prot/ProteinSubGrouping.py
+++ package_pout2prot/pout2prot/ProteinSubGrouping.py
@@ -1,53 +1,70 @@
-def create_protein_groups(protein_peptide_dict, peptide_protein_dict):
-    """creates protein groups, calls recursion_check_for_more_linked_proteins"""
-    # create dictionary to be returned, will map protein_group_id to list of protein_id
-    protein_groups = dict()
-    # protein_group_id is a simple integer
-    protein_group_id = 0
-    # create list of protein_id from proteins-dict which we will exhaust
-    remaining_proteins = set(protein_peptide_dict.keys())
-    # continue until no proteins remaining for processing
-    while len(remaining_proteins) > 0:
-        # take another protein
-        protein = remaining_proteins.pop()
-        # create new protein_group from current protein (i.e. entry in dict)
-        protein_group_id += 1
-        protein_groups[protein_group_id] = {protein}
-        # check for more linked proteins with recursive call
-        recursion_check_for_more_linked_proteins(protein_group_id, remaining_proteins, protein_peptide_dict,
-                                                 protein_groups, peptide_protein_dict)
+def anti_occam_create_protein_subgroups(protein_groups, protein_peptide_dict):
+    """takes the protein-groups and divides them into subgroups"""
+    # protein_subgroups
+    protein_subgroups = dict()
+    subgroup_count = 0
+    # loop through the protein groups
+    for protein_group_id in protein_groups.keys():
+        the_protein_list = list()
+        for protein in protein_groups[protein_group_id]:
+            the_protein_list.append(protein)
+        # dictionary of sets
+        protein_2_possible_grouping_set = dict()
+        for protein_1 in the_protein_list:
+            protein_1_grouping_set = set()
+            protein_1_grouping_set.add(protein_1)
+            for protein_2 in the_protein_list:
+                if protein_peptide_dict[protein_1].issubset(protein_peptide_dict[protein_2]) or \
+                        protein_peptide_dict[protein_2].issubset(protein_peptide_dict[protein_1]):
+                    if protein_1 != protein_2:
+                        protein_1_grouping_set.add(protein_2)
+            protein_2_possible_grouping_set[protein_1] = protein_1_grouping_set
 
-    # return the map protein_group_id -> list(protein_id)
-    return protein_groups
+        # if the value-set are equal among proteins they are a subgroup
+        already_used = list()
+        for protein_test1 in protein_2_possible_grouping_set.keys():
+            if protein_test1 not in already_used:
+                subgroup = list()
+                subgroup.append(protein_test1)
+                already_used.append(protein_test1)
+                for protein_test2 in protein_2_possible_grouping_set.keys():
+                    if protein_test2 not in already_used:
+                        if protein_2_possible_grouping_set[protein_test1] == protein_2_possible_grouping_set[protein_test2]:
+                            subgroup.append(protein_test2)
+                            already_used.append(protein_test2)
+
+                subgroup_count += 1
+                protein_subgroup_id_first_half = str(protein_group_id) + "_"
+                subgroup_id = protein_subgroup_id_first_half + str(subgroup_count)
+                # then update dictionary
+                protein_subgroups[subgroup_id] = subgroup
+
+    # voila!
+    return protein_subgroups
 
 
-def recursion_check_for_more_linked_proteins(protein_group_id, remaining_proteins, protein_peptide_dict,
-                                             protein_groups, peptide_protein_dict):
-    """recursive call, handles most of the grouping logic"""
-    # 1. create a set of proteins that are currently in the group
-    current_protein_set = set()
-    # 2. And collect all the peptides of these proteins into a set
-    peptide_set = set()
+def occam_create_protein_subgroups(protein_groups, protein_peptide_dict):
+    protein_subgroups = dict()
+    subgroup_count = 0
+    for protein_group_id in protein_groups.keys():
+        protein_subgroup_id_first_half = str(protein_group_id) + "_"
+        the_protein_list = list()
+        for protein in protein_groups[protein_group_id]:
+            the_protein_list.append(protein)
+        already_used = set()
+        for protein_1 in the_protein_list:
+            if protein_1 not in already_used:
+                subgroup = list()
+                subgroup.append(protein_1)
+                already_used.add(protein_1)
+                for protein_2 in the_protein_list:
+                    if protein_2 not in already_used:
+                        if protein_peptide_dict[protein_1] == protein_peptide_dict[protein_2]:
+                            subgroup.append(protein_2)
+                            already_used.add(protein_2)
+                subgroup_count += 1
+                subgroup_id = protein_subgroup_id_first_half + str(subgroup_count)
+                # then update dictionary
+                protein_subgroups[subgroup_id] = subgroup
 
-    for protein in protein_groups[protein_group_id]:
-        # go through list of peptides in the protein_peptide_dict and add them to set
-        current_protein_set.add(protein)
-        peptide_set.update(protein_peptide_dict[protein])
-
-    # 3. for all peptides in the peptide_set collect a new protein set, possibly catching new ones
-    new_protein_set = set()
-    for peptide in peptide_set:
-        new_protein_set.update(peptide_protein_dict[peptide])
-    # update the protein_groups-entry with the new list
-    protein_groups[protein_group_id].update(new_protein_set)
-    # update the remaining_proteins-list by removing the newly added proteins
-    for protein in new_protein_set:
-        if protein in remaining_proteins:
-            remaining_proteins.remove(protein)
-    # 4a. if current_protein_set is not equal to new_protein_set call recursion_check_for_more_linked_proteins again
-    if (len(current_protein_set.difference(new_protein_set)) != 0
-            or len(new_protein_set.difference(current_protein_set)) != 0):
-        # make recursive call
-        recursion_check_for_more_linked_proteins(protein_group_id, remaining_proteins, protein_peptide_dict,
-                                                 protein_groups, peptide_protein_dict)
-    # 4b. if current_protein_set is equal to new_protein_set, the group creation is finished
+    return protein_subgroups
